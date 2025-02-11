95cfa5e4576f33924046480b6d111ae2f767e5e4
Saurav Roy <srsaurav0@gmail.com>
2025-02-11 12:30:51 +0600

    # Feature - Router for all_params_API added

--- routers/router.go
+++ routers/router.go
@@ -1,4 +1,22 @@
-
 package routers
 
+import (
+	"flight_api/controllers"
 
+	"github.com/beego/beego/v2/server/web"
+)
+
+func init() {
+	// Initialize the Elasticsearch client (called once on app startup)
+	controllers.Init()
+
+	// Define the API namespace
+	ns := web.NewNamespace("/api/v1",
+		web.NSNamespace("/flights",
+			web.NSRouter("all_params/search", &controllers.FlightController{}, "get:GetByAllParams"),
+		),
+	)
+
+	// Register the namespace
+	web.AddNamespace(ns)
+}
