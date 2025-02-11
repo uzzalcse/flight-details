4612e0a741f4fb3540a9c6b0faedc8e35e9f059a
pritom006 <banerjeecse08@gmail.com>
2025-02-11 18:00:34 +0600

    # Improvement added router for dest time

--- routers/router.go
+++ routers/router.go
@@ -1,4 +1,24 @@
-
 package routers
 
+import (
+    "flight_api/controllers"
 
+    "github.com/beego/beego/v2/server/web"
+)
+
+func init() {
+    // Initialize the Elasticsearch client (called once on app startup)
+    controllers.Init()
+
+    // Define the API namespace
+
+    ns := web.NewNamespace("/api/v1",
+        web.NSNamespace("/flights",
+            web.NSRouter("/dest_time/search", &controllers.FlightController{}, "get:Get"),  // Corrected route
+        ),
+    )
+
+    // Register the namespace
+    web.AddNamespace(ns)
+	web.Router("/swagger/*", &controllers.SwaggerController{})
+}
